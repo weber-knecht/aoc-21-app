c229393887ce7ee5696e3906d56f847b699194b8
weber-knecht <weber.jakob@gmx.de>
2021-12-01 23:03:37 +0100

    added day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,36 @@ export class AppComponent {
   parsedInput: string[] = [];
 
   Day1Part1() {
-    this.parsedInput=(this.input.split(" "));
+    this.result = 0;
+    this.parsedInput=this.input.split(" ");
+    for (let index = 1; index < this.parsedInput.length; index++) {
+      let previousElement = this.parsedInput[index-1];
+      let currentElement = this.parsedInput[index];
 
-    for (let index = 0; index < this.parsedInput.length; index++) {
-      
-
+      if (+currentElement > +previousElement) {
+        this.result++;
+      }
     }
-
-
   }
+
+  Day1Part2() {
+    this.result = 0;
+    this.parsedInput=this.input.split(" ");
+
+    let prv = 0;
+    let cur = 0;
+    for (let i = 0; i < this.parsedInput.length; i++) {
+      cur = +this.parsedInput[i]
+      for (let j = i+1; j < i + 3; j++) {
+        cur += +this.parsedInput[j]
+      }
+      console.log(cur + " > " + prv);
+      if (cur > prv && prv != 0) {
+        this.result++;
+      }
+      prv = cur;
+      cur = 0;
+    }
+  }
+
 }
