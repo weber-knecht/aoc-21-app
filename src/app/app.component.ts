61cbefb3332cea4527dc9a4d476b6c995a2e710d
weber-knecht <weber.jakob@gmx.de>
2021-12-01 22:00:23 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aoc-app';
+  result: number = 0;
+  input: string;
+  parsedInput: string[] = [];
+
+  Day1Part1() {
+    this.parsedInput=(this.input.split(" "));
+
+    for (let index = 0; index < this.parsedInput.length; index++) {
+      
+
+    }
+
+
+  }
 }
